503fe75ce36decffe31f2d70c8039941ade2c075
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-19 08:11:36 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -11,13 +11,14 @@
     "website": "https://gestpro.cloud",
     "license": "LGPL-3",
     "depends": ["stock", "base", "web"],  # Asegúrate de incluir "web" para cargar el módulo JS correctamente.
-    "data": [
-        "views/stock_picking_views.xml",
-        "views/rollo_menu_views.xml",
-        "views/stock_lot_views.xml",
-        "report/rollo_label_template.xml",
-        "security/ir.model.access.csv"  # Incluye este archivo si es necesario.
-    ],
+   "data": [
+            "views/stock_lot_views.xml",  # Este archivo debe cargarse primero para que las acciones estén definidas
+            "views/rollo_menu_views.xml",  # Ahora puedes cargar el menú que hace referencia a las acciones definidas anteriormente
+            "views/stock_picking_views.xml",
+            "report/rollo_label_template.xml",
+            "security/ir.model.access.csv"  # Incluye este archivo si es necesario.
+],
+
     "assets": {  # La nueva estructura de assets.
         "web.assets_backend": [
             "rollo_propiedades_unicas/static/src/js/rollo_properties.js",  # Asegúrate de que las rutas sean correctas.
